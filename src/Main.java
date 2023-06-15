1fae4180cb2e2bcb83b139b2a80542492091d5d5
rm86330 <rm86330@estudante.fieb.edu.br>
2023-06-15 12:28:59 -0300

    15/06

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,20 @@ public class Main {
                 p2 = new Pessoa(); //construção
         //...
         p1.nome = "Elliot Page";
+        p1.cpf = "495.493.492-91";
+        p1.telfami="(69) 95202-2588";
+        p1.cep="02266-303";
+        p1.tel="(69) 98645-5344";
+        p1.rg="24.556.439-4";
 
+        p2.nome = "Daniel Alves";
+        p2.cpf = "369.475.246-01";
+        p2.telfami="(69) 95202-2588";
+        p2.cep="02266-303";
+        p2.tel="(69) 98954-5344";
+        p2.rg="27.555.349-8";
 
+        p3.nome = "Daniel Alves";
+        p3.cpf = "369.475.246-01";
     }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,6 +1,15 @@
 public class Pessoa {
     //Atributos
     String nome;
+    String cpf;// 111.111.111.-11
+    String telfami;
+    String tel;
+    String rg;
+    String  cep;
+
+
+
+
     //Métodos
     public void pesquisarEmpresas(){
 
